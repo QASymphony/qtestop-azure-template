f8e28dc86f6adeeceeab53d263543765110314a0
Huy Nguyen <huynguyen@qasymphony.com>
2020-06-22 11:14:57 -0400

    code commit

---
+++ utils/logger.js
@@ -0,0 +1,29 @@
+const winston = require('winston');
+const { combine, timestamp, colorize } = winston.format;
+
+const transformer = {
+  transform: (info) => {
+    if (info.message) {
+      info[Symbol.for("message")] = `${info.timestamp} - [${info.level}]: ${info.message.replace('\n', '')}`;
+    }
+    return info;
+  }
+};
+
+const logFormat = combine(timestamp(), transformer);
+const consoleFormat = combine(colorize(), logFormat);
+
+exports.logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      format: consoleFormat,
+      level: 'info',
+      handleExceptions: true
+    }),
+    new winston.transports.File({
+      format: logFormat,
+      filename: 'app.log',
+      level: 'info'
+    })
+  ]
+});
